1df4d4cedce07e38bf2d83ba4d885e3bff95a3fb
Janne Salokoski <salokoski.janne@gmail.com>
2021-03-18 11:51:12 +0200

    Initialize empty project

---
+++ index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from '~/src/App'
+
+function enable_hmr() {
+	if (module.hot) {
+		module.hot.accept()
+	}
+}
+
+function render_application() {
+	ReactDOM.render(
+		<App />,
+		document.getElementById('root')
+	)
+}
+
+function main() {
+	enable_hmr()
+	render_application()
+}
